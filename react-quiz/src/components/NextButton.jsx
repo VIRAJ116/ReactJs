2f401124d6f0aafc12025349784103a5bd4e683b
Viraj Raiyani <viraj.raiyani@brainvire.com>
2023-12-28 23:50:30 +0530

    [Fixed finish btn not showing issue]

--- react-quiz/src/components/NextButton.jsx
+++ react-quiz/src/components/NextButton.jsx
@@ -8,7 +8,7 @@ function NextButton({ dispatch, answer, index, numQuestions }) {
       Next
     </button>
   );
-  if (index === numQuestions) return (
+  if (index === numQuestions - 1) return (
     <button className="btn btn-ui" onClick={() => dispatch({ type:'finish' })}>Finish</button>
   )
 }
