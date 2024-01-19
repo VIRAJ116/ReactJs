7e938c392a7341fabf551aabb40ca423ee2ca319
Viraj Raiyani <viraj.raiyani@brainvire.com>
2024-01-19 19:24:07 +0530

    Finished app

--- worldswise/src/components/CountryList.jsx
+++ worldswise/src/components/CountryList.jsx
@@ -1,14 +1,17 @@
-import Spinner from './Spinner';
-import styles from './CountryList.module.css';
-import CountryItem from './CountryItem';
-import Message from './Message';
+import Spinner from "./Spinner";
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
     return (
-      <Message message='Add your first city by clicking on a city on the map' />
+      <Message message="Add your first city by clicking on a city on the map" />
     );
 
   const countries = cities.reduce((arr, city) => {
