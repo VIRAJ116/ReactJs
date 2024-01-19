2b7078278f2346e0fba680fc84616a4d3265cfed
Viraj Raiyani <viraj.raiyani@brainvire.com>
2024-01-19 19:22:02 +0530

    Finished reacr quiz app

--- react-quiz/src/components/Progress.jsx
+++ react-quiz/src/components/Progress.jsx
@@ -1,11 +1,22 @@
-function Progress({ index, numQuestions, maxPossiblePoints, points, answer }) {
-    return (
-        <header className="progress">
-            <progress max={numQuestions} value={index + Number(answer !== null)}></progress>
-            <p>Question <strong>{index+1}</strong> / {numQuestions}</p>
-            <p><strong>{points}</strong> / {maxPossiblePoints}</p>
-        </header>
-    )
+import { useQuiz } from '../contexts/QuizContext';
+
+function Progress() {
+  const { index, numQuestions, points, maxPossiblePoints, answer } = useQuiz();
+
+  return (
+    <header className='progress'>
+      <progress
+        max={numQuestions}
+        value={index + Number(answer !== null)}
+      ></progress>
+      <p>
+        Question <strong>{index + 1}</strong> / {numQuestions}
+      </p>
+      <p>
+        <strong>{points}</strong> / {maxPossiblePoints}
+      </p>
+    </header>
+  );
 }
 
-export default Progress
+export default Progress;
