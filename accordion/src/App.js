1799df02f7504229f9e7328ac26a383f80c2fcf6
Viraj <viraj.raiyani@brainvire.com>
2024-04-27 20:57:37 +0530

    transition changes

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -53,14 +53,14 @@ function AccordionItem({ num, title, curOpen, onOpen, children }) {
 
   return (
     <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
-      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
-      <p className="title">{title}</p>
-      <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && (
-        <div className="content-box">
+      <div className="heading-wrapper">
+        <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+        <p className="title">{title}</p>
+        <p className="icon">{isOpen ? "-" : "+"}</p>
+      </div>
+        <div className={`content-box`}>
           {children}
         </div>
-      )}
     </div>
   );
 }
