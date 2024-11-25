e25c9230fbd8b3880e9efbe0da2d51f37be152ee
Viraj Raiyani <virajraiyani162@gmail.com>
2024-11-26 00:25:40 +0530

    Learning Next Js

--- the-wild-oasis-website/app/layout.js
+++ the-wild-oasis-website/app/layout.js
@@ -1,3 +1,4 @@
+import Logo from "./components/Logo";
 import Navigation from "./components/Navigation";
 
 export const metadata = {
@@ -8,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Logo />
         <Navigation />
         <main>{children}</main>
         <footer>Copyright by the Wild Oasis</footer>
